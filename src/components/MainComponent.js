20207c6c76913baa942666f9807497010ce509b9
Andrew Sutton <andrewsydsutton@gmail.com>
2021-12-17 09:00:25 -0600

    jumbptron

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,54 +1,31 @@
 import React from "react"
+import city from "../images/City.jpg"
 import {
-    Card
 } from "reactstrap"
 
 const MainComponent = () => {
     return (
-        <div className="main-container container text-center">
-            <div className="row border border-warning">
-                <div id="top-card" className="col">
-                    <div className="media border border-success text-light">
-                        <img alt="Myself" src="https://thepowerofthedream.org/wp-content/uploads/2015/09/generic-profile-picture-1200x1200.jpg" height="100" width="100" className="rounded-circle thumbnail"/>
-                        <div className="media-body border border-primary">
-                            <p>hello I'm</p>
-                            <h3>Andrew Sutton</h3>
-                            <a href="linkedin.com">LinkedIn</a>
-                            <a href="GitHub.com">GitHub</a>
-                            <a href="gmail.com">Gmail</a>
-                            <a href="" className="btn btn-danger">Resume</a>
+        <div className="text-center">
+            <div className="jumbotron">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <div id="circle-1"></div>
+                            <div id="circle-2"></div>
+                            <div id="circle-3"></div>
+                            <div id="circle-4"></div>
+                            <img src={city} alt="Me" id="img" height="300" width="300" className="rounded-circle" />
+                        </div>
+                        <div className="col-md-8 text-light">
+                            <h1 id="name-heading">Andrew Sutton</h1>
+                            <h3>Front End Web Developer</h3>
+                            <div className="button-group mt-5">
+                                <a href="/" className="btn btn-info">Resume</a>
+                                <a href="/" className="btn btn-info">LinkedIn</a>
+                                <a href="/" className="btn btn-info">GitHub</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div className="row border border-danger text-center">
-                <div className="col border border-warning">
-                    <h3>Projects</h3>
-                    <Card>
-                        <h2>Here's one project</h2>
-                    </Card>
-                    <Card>
-                        <h2>Here's a second project</h2>
-                    </Card>
-                    <Card>
-                        <h2>Here's a third project</h2>
-                    </Card>
-                </div>
-            </div>
-            <div className="row border border-danger text-center">
-                <div className="col border border-warning">
-                    <h3>Photo Editing/ Photography</h3>
-                    <Card>
-                        <h2>Here's a gallery</h2>
-                    </Card>
-                </div>
-            </div>
-            <div className="row border border-danger text-center">
-                <div className="col border border-warning">
-                    <h3>Music</h3>
-                    <Card>
-                        <h2>Here's some songs</h2>
-                    </Card>
                 </div>
             </div>
         </div>
