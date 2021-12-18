ed6505b6287c4fc76b10f40cb66f341cef797a07
Andrew Sutton <andrewsydsutton@gmail.com>
2021-12-18 08:45:16 -0600

    12.18

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,14 +1,14 @@
 import React from "react"
-import city from "../images/City.jpg"
-import yourshelf from "../images/YourShelf.png"
+import city from "../images/me.png"
 import {
     Card
 } from "reactstrap"
+import projects from "../projects"
 
 const MainComponent = () => {
     return (
         <div className="text-center">
-            <div className="jumbotron border border-warning">
+            <div className="jumbotron">
                 <div className="container">
                     <div className="row">
                         <div className="img-col col-lg-4">
@@ -22,38 +22,57 @@ const MainComponent = () => {
                             <h1 id="name-heading">Andrew <strong>Sutton</strong></h1>
                             <h4 className="text-dark">Front End Web Developer</h4>
                             <div className="btn-group mt-3">
-                                <a href="/" className="btn text-light">Resume</a>
-                                <a href="/" className="btn text-light">LinkedIn</a>
-                                <a href="/" className="btn text-light">GitHub</a>
+                                <a className="btn text-light" href="/">Resume</a>
+                                <a className="btn text-light" href="https://www.linkedin.com/in/andrew-sutton-aa3326205/">LinkedIn</a>
+                                <a className="btn text-light" href="/">GitHub</a>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="container mt-3">
-                <div className="row">
-                    <div className="col-sm-12">
-                        <h3>Projects</h3>
-                    </div>
-                </div>
-                <Card>
+                <div className="container mt-5">
                     <div className="row">
-                        <div className="col-sm-4">
-                            <img src={yourshelf} alt="A book search application called yourshelf" height="400" />
-                        </div>
-                        <div className="col-sm-4 text-left">
-                            <h3>YourShelf</h3>
-                            <ul>
-                                <li>User can search for any book by title or by subject</li>
-                                <li>User can save books in their digital bookshelf</li>
-                            </ul>
-                            <div className="btn-group ">
-                                <a className="btn text-light" href="/">demo</a>
-                                <a className="btn text-light" href="/">repo</a>
-                            </div>
+                        <div className="col-sm-12">
+                            <h2>Projects</h2>
                         </div>
                     </div>
-                </Card>
-            </div>
+                    <div className="row">
+                        <div className="col-xl-6">
+                            {projects.map(project => {
+                                return (
+                                <Card className="mt-4 p-3">
+                                    <div className="row">
+                                        <div className="col-sm-4">
+                                            <img src={project.img} alt="A book search application called yourshelf" height="400" />
+                                        </div>
+                                        <div className="col-sm-8 text-left">
+                                            <h4 className="font-weight-bold">{project.title}</h4>
+                                            <p>{project.description}</p>
+                                            <h5>Technology Used</h5>
+                                            <ul>
+                                                {project.tech.map(tech => {
+                                                    return (
+                                                        <li>{tech}</li>
+                                                    )
+                                                })
+                                            }
+                                            </ul>
+                                            <div className="btn-group">
+                                                <a className="btn text-light" href="/">demo</a>
+                                                <a className="btn text-light" href={project.repo}>repo</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Card>
+                                )
+                            })}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
---
+++ src/projects.js
@@ -0,0 +1,41 @@
+const projects = [
+    {
+        title: "YourShelf",
+        img: "./images/YourShelf.png",
+        alt: "A book search application called yourshelf",
+        description: "A book application where the user can search for any book, either by title or subject. After loggin in to their account, the user can then save any amount of books in their digital bookshelf.",
+        tech: [
+            "React JS",
+            "React Native",
+            "API's",
+            "Redux",
+            "React Native Elements",
+            "Adobe Photoshop"
+        ],
+        repo: "https://github.com/sydsutton/book-app-native"
+    },
+    {
+        title: "Cook It Yourself",
+        description: "A fully responsive single page application (SPA) where a user can find recipes for vaious meals. Error handling for the login and feedback forms were implemented using Javascript.",
+        tech: [
+            "React JS",
+            "Bootstrap",
+            "JSON",
+            "React Router"
+        ],
+        repo: "https://github.com/sydsutton/ReactRecipeApp",
+        live: "https://ciy-app.herokuapp.com/"
+    },
+    {
+        title: "Locally",
+        description: "A fully responsive website that, if fully functional, would allow a user to search for any band/artist within a certain mile radius of the user's location.",
+        tech: [
+            "HTML/ CSS",
+            "Bootstrap",
+            "JSON"
+        ],
+        repo: "https://github.com/sydsutton/ReactRecipeApp"
+    },
+]
+
+export default projects
