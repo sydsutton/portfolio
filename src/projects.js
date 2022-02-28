dd23e63caa8d3ad9aaca29990a79182ab300ce3b
Andrew Sutton <andrewsydsutton@gmail.com>
2022-02-28 09:20:46 -0600

    added band site to projects

--- src/projects.js
+++ src/projects.js
@@ -5,8 +5,36 @@ import portfolio from "./images/portfolio.png"
 import weather from "./images/weather.png"
 import newz from "./images/newz.PNG"
 import sthetik from "./images/sthetik.PNG"
+import syk from "./images/syk.PNG"
 
 const projects = [
+    {
+        title: "Soul You Know Site",
+        img: `${syk}`,
+        alt: "A band website",
+        description: "The official site of the band 'Soul You Know'. It has a working mailing list submission form and a contact form that goes directly to the band's email.",
+        tech: [
+            "Javascript",
+            "React JS",
+            "Semantic UI",
+            "SCSS",
+            "Session Storage",
+            "React Router",
+            "Adobe Photoshop",
+            "Email JS",
+            "Mailchimp JS"
+        ],
+        problem: [
+            "I wanted the mailing list form to pop up only once per site visit instead of every time a page loads",
+            "I needed a way to search for photos based on the 'type' of photo (live shows, album art, etc.)",
+        ],
+        solution: [
+            "I accessed session storage and set a 'timeout' function so that the form pops up once after 4 seconds of your first site visit",
+            "I saved all of the photos in an array with a 'type' key in order to filter through the photos based on the type selected on the page",
+        ],
+        repo: "https://github.com/sydsutton/soul-you-know",
+        live: "https://soulyouknowband.com"
+    },
     {
         title: "SThetik",
         img: `${sthetik}`,
